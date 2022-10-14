bbcbda76286b015007bed96ac43ffe491c2e9134
theotherhades <64885838+theotherhades@users.noreply.github.com>
2022-10-14 21:08:36 +1300

    Added clan command

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import os
 import nextcord
+from pyterri import clan
 from nextcord import Interaction, SlashOption, ChannelType
 from nextcord.abc import GuildChannel
 from nextcord.ext import commands
 
 client = commands.Bot()
 
-GUILD_ID = 785366147331129355
+GUILD_IDS = list().append(i.id for i in client.guilds)
 
 @client.event
 async def on_ready():
     print("bot is online")
 
-@client.slash_command(name = "help", description = "A good place to get started with the scorekeeper", guild_ids = [GUILD_ID])
+@client.slash_command(name = "help", description = "A good place to get started with the scorekeeper", guild_ids = GUILD_IDS)
 async def ping(interaction: Interaction):
     embed = nextcord.Embed(title = "Help", description = "empty for now")
+    await interaction.response.send_message(embed = embed)
+
+@client.slash_command(name = "clan", description = "Get score and rank for a clan", guild_ids = GUILD_IDS)
+async def clan(interaction: Interaction, arg: str):
+    try:
+        data = clan.getClan(arg)
+        embed = nextcord.Embed(name = arg.upper(), description = f"**Score:** {data['score']}", color = nextcord.Color.blue())
+    except IndexError:
+        embed = nextcord.Embed(name = arg.upper(), description = "No data was found for this clan, most likely because it does not have a recorded score.", color = nextcord.Color.red())
+
     await interaction.response.send_message(embed = embed)
 
 client.run(os.environ["CLIENT_TOKEN"])
